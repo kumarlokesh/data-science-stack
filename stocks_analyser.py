550555c40edb66bcbe286483b7ca6fa99370d59f
Lokesh Kumar <lokesh.kumar@gramvaani.org>
2017-10-16 05:15:27 +0530

    Bollinger bands and daily/cumulative returns

--- stocks_analyser.py
+++ stocks_analyser.py
@@ -1,7 +1,5 @@
 """
-Script to read stocks information from csv files.
-Capability to slice and plot data using pandas and
-matplotlib.
+Calculate Bollinger bands.
 """
 
 import os
@@ -43,11 +41,43 @@ def plot_data(df, columns, start_index, end_index, title):
     plt.show()
 
 
+def get_rolling_mean(values, window):
+    return values.rolling(window=window,center=False).mean()
+
+
+def get_rolling_std(values, window):
+    return values.rolling(window=window,center=False).std()
+
+
+def get_bollinger_bands(rm, rstd):
+    return (rm + (rstd*2)), (rm - (rstd*2))
+
+
 def main():
     dates = pd.date_range('2010-01-01', '2010-12-31')
     stock_symbols = ['GOOG', 'IBM', 'GLD']
     df = get_stock_data(stock_symbols, dates)
-    plot_data(df, ['SPY', 'IBM'], '2010-03-01', '2010-04-01', "Stock prices")
+    # plot_data(df, ['SPY', 'IBM'], '2010-03-01', '2010-04-01', "Stock prices")
+
+     # Compute Bollinger Bands
+    # 1. Compute rolling mean
+    rm_SPY = get_rolling_mean(df['SPY'], window=20)
+
+    # 2. Compute rolling standard deviation
+    rstd_SPY = get_rolling_std(df['SPY'], window=20)
+
+    # 3. Compute upper and lower bands
+    upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
+
+    # Plot raw SPY values, rolling mean and Bollinger Bands
+    ax = df['SPY'].plot(title="Bollinger Bands", label='SPY')
+    rm_SPY.plot(label='Rolling mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Prices")
+    ax.legend(loc='upper left')
+    plt.show()
 
 
 if __name__ == "__main__":
